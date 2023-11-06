c6d547296dea5b3dba9208850380a7766beeec91
nguyencunq <cungptit01@gmail.com>
2023-11-06 12:55:19 +0700

    Added my project

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker rm -f lovetok
 docker build -t lovetok . && \
-docker run --name=lovetok --rm -p1337:80 -d -it lovetok
+docker run --name=lovetok --rm -p9000:80 -d -it lovetok
 docker logs -f lovetok &> /var/log/access.log
